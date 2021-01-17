b231ba397bf9d630d1ee8b1030e531e00f8b9d20
6kamens <slil.puangpoom@gmail.com>
2021-01-17 12:34:51 +0700

    initial branch

--- Javascript/Advance_Javascript/advance_js_2.js
+++ Javascript/Advance_Javascript/advance_js_2.js
@@ -1,4 +1,17 @@
-import {sayHi, sayBye} from './advance_js_1';
+//11. จงหาจำนวนเลขโดดตั้งแต่ 1 ถึง 4,129,980
 
-sayHi('John'); // Hello, John!
-sayBye('John'); // Bye, John!
+const sumDigit =(firstNum,lastNum)=>{
+
+    let result = 0 ;
+  
+    for(let i= firstNum; i <= lastNum ; i++){
+      result  = result + sumInput(i) ;
+    }
+  
+    return  result;
+  
+  }
+  
+  console.log('\nข้อ 15');
+  console.log('input 1 - 4129980');
+  console.log(sumDigit(1,4129980));
--- Javascript/advance_js_2.js
+++
@@ -1,4 +0,0 @@
-import {sayHi, sayBye} from './advance_js_1';
-
-sayHi('John'); // Hello, John!
-sayBye('John'); // Bye, John!
