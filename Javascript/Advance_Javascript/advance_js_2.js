9e1a0ca1f3bd3fc86099cfabf14c6a131291bb9b
6kamens <slil.puangpoom@gmail.com>
2021-01-17 12:59:54 +0700

    problem 1

--- Javascript/Advance_Javascript/advance_js_2.js
+++ Javascript/Advance_Javascript/advance_js_2.js
@@ -1,17 +1,30 @@
-//11. จงหาจำนวนเลขโดดตั้งแต่ 1 ถึง 4,129,980
+//1. ให้เขียนฟังก์ชัน checkCharacter(string) โดยคืนค่าเป็น stringโดยที่ 
+// ถ้า string ที่เข้ามา เป็น พิมพ์ใหญ่ทั้งหมด ให้คืนค่าเป็น string ที่มีค่าเป็น “All character are capital.”
+// ถ้า string ที่เข้ามา เป็น พิมพ์เล็กทั้งหมด ให้คืนค่าเป็น string ที่มีค่าเป็น “All character are small.”
+// ถ้า string ที่เข้ามา เป็น พิมพ์ใหญ่และพิมเล็กผสมกัน ให้คืนค่าเป็น string ที่มีค่าเป็น “All character are mix.”
 
-const sumDigit =(firstNum,lastNum)=>{
 
-    let result = 0 ;
-  
-    for(let i= firstNum; i <= lastNum ; i++){
-      result  = result + sumInput(i) ;
+const checkCharacter =(word)=>{
+
+    let haveLowerCase = false;
+    let haveUpperCase = false;
+    
+    for(let i= 0; i < word.toString().length ; i++){
+       if(word.toString()[i] == word.toString()[i].toUpperCase()) haveUpperCase = true;
+       if(word.toString()[i] == word.toString()[i].toLowerCase()) haveLowerCase = true;
     }
   
-    return  result;
+   
+    if(haveLowerCase && haveUpperCase) return 'All character are mix.';
+    if(haveLowerCase) return 'All character are small.';
+    if(haveUpperCase) return 'All character are capital.';
   
-  }
+}
   
-  console.log('\nข้อ 15');
-  console.log('input 1 - 4129980');
-  console.log(sumDigit(1,4129980));
+  console.log('\nข้อ 1');
+  console.log('input 1 - STRING');
+  console.log(checkCharacter('STRING'));
+  console.log('input 2 - string');
+  console.log(checkCharacter('string'));
+  console.log('input 3 - StrIng');
+  console.log(checkCharacter('StrIng'));
